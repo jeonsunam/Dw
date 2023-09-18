c8f526ab4d5c6c0ab7dcfa5e4c2d12a5087490f6
jeonsunam <sunam0307@naver.com>
2023-09-18 17:28:47 +0900

    javascript prompt

---
+++ 230918/js/script.js
@@ -0,0 +1,13 @@
+const userName = document.getElementById('username')
+console.log(userName)
+// userName의 텍스트 내용을 prompt에서 받은 값으로 교체
+// prompt('사용자에게 보여줄 메세지', '기본값')
+let value = prompt('이름을 입력해주세요', '멍멍이');
+userName.textContent = value;
+// userName의 컬러를 변경해주세요
+let color = prompt('원하는 컬러를 입력해주세요', 'dodgerblue');
+userName.style.color = color;
+// 폰트 사이즈 값을 받아 적용해보세요
+let font = prompt('원하는 폰트 크기를 입력해주세요', '40px');
+userName.style.fontSize = font;
+
