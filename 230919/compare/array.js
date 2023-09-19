4ca00d9465e85f733a301daf2c1b270e2bb13cc0
jeonsunam <sunam0307@naver.com>
2023-09-19 11:20:05 +0900

    배열

---
+++ 230919/compare/array.js
@@ -0,0 +1,18 @@
+// Array.js
+const pokemon = [`피카츄`, `이브이`, `라이츄`, `파이리`, `꼬부기`];
+// push는 배열 마지막에 요소를 추가
+pokemon.push(`망나뇽`);
+
+// 포켓몬 배열에 이상해씨를 추가해주세요
+pokemon.push(`이상해씨`);
+
+// 배열에 마지막 요소를 제거
+pokemon.pop();
+
+console.log(pokemon);
+
+// [].length : 배열의 길이를 알아보는 방법
+console.log(pokemon.length);
+console.log(pokemon[0]); // 피카츄
+// 파이리를 출력해주세요 
+console.log(pokemon[3]);
